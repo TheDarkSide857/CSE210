ffc6ba718a77a5040b85011cbed48b2161085a1a
Dallin Stewart <indominusdallin@icloud.com>
2026-02-12 07:33:37 -0800

    Add files via upload

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Red", 3);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Blue", 4, 5);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Green", 6);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape: {shape.GetType().Name}, Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
